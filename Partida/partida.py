44080e0dcd6eee5b2954f2b75960e5243e9895af
jvsc01 <jvsc01@live.com>
2020-10-12 14:50:59 -0300

    partida adicionando

--- Partida/partida.py
+++ Partida/partida.py
@@ -5,4 +5,12 @@ from random import *
 
 def define_ordem_jogadores(lista_jogadores):
     shuffle(lista_jogadores)
+def cria_partida():
+    n=set.numerojogadores()
+    for n in range(0,n-1):
+        nome=set_nome() #CRIAR SET_NOME
+        jogadores.append=jogador.cria_jogador(-1,nome)
+    tabuleiro.criar_tabuleiro(n)
+    return
+
 
