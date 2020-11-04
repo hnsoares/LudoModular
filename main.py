61efb847095fa559d698b003ca9b952fd82c4a39
MarcosViniciusAraujo <m.v.araujo.almeida@gmail.com>
2020-11-04 15:45:29 -0300

    Adicionei os botões às posições na tela, trabalhando nas interações com os botões
    Signed-off-by: MarcosViniciusAraujo <m.v.araujo.almeida@gmail.com>

--- main.py
+++ main.py
@@ -1,79 +1,49 @@
 """
 Main da build #1
 """
-
-
 import pygame
-#partida.rodar_partida()
+from display.menu import Button
+
+
+#iniciar modulos de terceiros
 pygame.init()
 
+#constantes para tela
 X_SCREEN = 800
 Y_SCREEN = 600
-screen = pygame.display.set_mode((X_SCREEN,Y_SCREEN))#tamanho
-pygame.display.set_caption("LUDO") #titulo
 
-x = 10
-y = 500
-width = 40
-height = 60
-vel = 10
+#constantes para o botão
+HEIGHT_BUTTON = 50
+WIDTH_BUTTON = 150
+
+#pygame
+tela = pygame.display.set_mode((X_SCREEN, Y_SCREEN))#tamanho
+pygame.display.set_caption("LUDO")
+iniciar = Button('red', 300, 300, WIDTH_BUTTON, HEIGHT_BUTTON, 'Iniciar')
+config = Button('blue', 300, 370, WIDTH_BUTTON, HEIGHT_BUTTON, 'Configurações')
+fechar = Button('purple', 300, 440, WIDTH_BUTTON, HEIGHT_BUTTON, 'Fechar')
+
 running = True
-
-
-class Button():
-    def __init__(self, color, x, y, width, height, text=''):
-        self.color = color
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-
-    def draw(self, win, outline=None):
-        # Call this method to draw the button on the screen
-        if outline:
-            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
-
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', 60)
-            text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (
-            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
-
-    def isOver(self, pos):
-        # Pos is the mouse position or a tuple of (x,y) coordinates
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-
-        return False
-
-button = Button('blue', 100, 100, 100, 40, 'Teste')
 while running:
-    pygame.time.delay(50)
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
-    button.draw(screen)
-    pygame.display.update()
+    tela.fill((255, 255, 255))
 
+    iniciar.draw(tela)
+    config.draw(tela)
+    fechar.draw(tela)
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            print('Jogo finalizado')
+        #preciso clicar primeiro
         pos = pygame.mouse.get_pos()
-        if button.isOver(pos):
-            print('Clicou no botão')
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and x <= X_SCREEN - width:
-        x += vel
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
-
+        if iniciar.isOver(pos):
+            print('Iniciando o jogo...')
+        if config.isOver(pos):
+            print('Abrindo as configurações...')
+        if fechar.isOver(pos) and pygame.mouse.get_pressed():
+            running = False
+            print('Jogo finalizado')
 
 
 pygame.quit()
